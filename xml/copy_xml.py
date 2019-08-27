2e48858eb62efe21dc883a89e7b6eae63043106b
yinzhipeng123 <english126paopao@163.com>
2019-08-27 16:56:19 +0800

    复制脚本完成

--- xml/copy_xml.py
+++ xml/copy_xml.py
@@ -1,4 +1,13 @@
 import shutil
+import configparser
+
+conf = configparser.ConfigParser()
+conf.read("copy_xml_conf.conf")
+
+target =conf.get("target", "target") #目的目录
+
+filelist = conf.items("which")
+for i in filelist:
+    shutil.copy(i[1],target)
 
 
-shutil.copy()
