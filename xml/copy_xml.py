9a8d2f24f85a9c2829eb3e0b13651647e568a48d
yinzhipeng123 <english126paopao@163.com>
2019-08-28 10:16:33 +0800

    复制脚本完成

--- xml/copy_xml.py
+++ xml/copy_xml.py
@@ -1,13 +1,15 @@
 import shutil
 import configparser
+import subprocess
 
 conf = configparser.ConfigParser()
 conf.read("copy_xml_conf.conf")
 
 target =conf.get("target", "target") #目的目录
 
+
 filelist = conf.items("which")
 for i in filelist:
-    shutil.copy(i[1],target)
-
-
+    # shutil.copy(i[1],target)
+    print('sshpass -p '+ password + ' scp '+i[1].split(',')[0]+' '+target)
+    subprocess.call('sshpass -p '+ password + ' scp '+i[1].split(',')[0]+' '+target, shell=True)
